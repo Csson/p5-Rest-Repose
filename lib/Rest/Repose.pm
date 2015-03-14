3b68bb23123ce148c4299a2d566c203e680b358c
Erik Carlsson <info@code301.com>
2015-03-14 23:50:52 +0100

    Init

---
+++ lib/Rest/Repose.pm
@@ -0,0 +1,29 @@
+use 5.10.1;
+use strict;
+use warnings;
+
+package Rest::Repose;
+
+# VERSION
+# ABSTRACT: Short intro
+
+
+
+1;
+
+
+__END__
+
+=pod
+
+=head1 SYNOPSIS
+
+    use Rest::Repose;
+
+=head1 DESCRIPTION
+
+Rest::Repose is ...
+
+=head1 SEE ALSO
+
+=cut
